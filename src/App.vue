5b573acfa5030113de1d4a0522592818647c0d86
FToovvr <FToovvr@protonmail.com>
2021-09-25 23:41:20 +0800

    refactor: 换种方式除掉烦人的对于 font 标签的警告

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,10 @@
 // https://github.com/vueuse/head
 // you can use this to manipulate the document head in any components,
 
+// 让 Vue 不要再对 font 标签发出警告了
+import { getCurrentInstance } from '@vue/runtime-core'
+getCurrentInstance()!.appContext.config.compilerOptions.isCustomElement = (tag) => tag === 'font'
+
 import { initializeStuffStore } from "./stores/stuff";
 
 // they will be rendered correctly in the html results with vite-ssg
