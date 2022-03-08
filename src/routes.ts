4d43bf58478d3bceebf5d3d904778d937e33183b
FToovvr <FToovvr@protonmail.com>
2022-03-08 22:39:40 +0800

    style: deno fmt src/{routes.ts,types/post.ts}

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,8 @@
-import { RouteRecordRaw } from 'vue-router'
-import { baseUrl } from './env'
+import { RouteRecordRaw } from "vue-router";
+import { baseUrl } from "./env";
 
-import Page from './pages/QuestThreadPage.vue'
+import Page from "./pages/QuestThreadPage.vue";
 
 export default [
-    { path: `${baseUrl}/folder-:folder/quest-:quest/:page`, component: Page },
-] as RouteRecordRaw[]
+  { path: `${baseUrl}/folder-:folder/quest-:quest/:page`, component: Page },
+] as RouteRecordRaw[];
